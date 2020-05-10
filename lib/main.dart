ed8aab57ed52ad47619f8e1f5992b2dace9a2d63
Алиев Турач Сураддин Оглы <taliev@stream.ru>
2020-05-10 20:48:22 +0300

    create new app

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorit color?',
+      'What\'s yout favorit animal?'
+    ];
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(title: Text('My first App')),
+            body: Column(
+              children: [
+                Text(questions[_questionIndex]),
+                RaisedButton(
+                  child: Text('Answer 1'),
+                  onPressed: _answerQuestion,
+                ),
+                RaisedButton(child: Text('Answer 2'), onPressed: () => print('Answer 2 chosen!')),
+                RaisedButton(child: Text('Answer 3m'), onPressed: () {
+                  print('Answer 3 chosen!');
+                }),
+              ],
+            )));
+  }
+}
