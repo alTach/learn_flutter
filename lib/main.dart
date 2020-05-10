a51b495022196c0ebf612dbd1222d9570d2d7648
Алиев Турач Сураддин Оглы <taliev@stream.ru>
2020-05-10 21:57:54 +0300

    create custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './question.dart';
 
 void main() {
   runApp(MyApp());
@@ -33,7 +34,7 @@ class _MyAppState extends State<MyApp> {
             appBar: AppBar(title: Text('My first App')),
             body: Column(
               children: [
-                Text(questions[_questionIndex]),
+                Question(questions[_questionIndex]),
                 RaisedButton(
                   child: Text('Answer 1'),
                   onPressed: _answerQuestion,
