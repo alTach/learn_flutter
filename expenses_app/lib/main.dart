f6de7e244f5c3454442810acf5cc5c62c5a22561
Алиев Турач Сураддин Оглы <taliev@stream.ru>
2020-05-19 22:02:58 +0300

    transaction data; UI; UI style; string interpolation; date format pattern;

--- expenses_app/lib/main.dart
+++ expenses_app/lib/main.dart
@@ -1,4 +1,6 @@
+import './transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,6 +15,16 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't2',
+        title: 'Weekly Groseries',
+        amount: 16.53,
+        date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,17 +32,49 @@ class MyHomePage extends StatelessWidget {
         title: Text('Flutter App'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Container(
             width: double.infinity,
             child: Card(
               color: Colors.blue,
               child: Text('CHART!'),
-              elevation: 50,
+              elevation: 50, // box-shadow
             ),
           ),
-          Card(
-            child: Text('List of TX'),
+          Column(
+            children: transactions
+                .map((tx) => Card(
+//              color: Colors.red,
+                      child: Row(
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.symmetric(
+                                vertical: 10, horizontal: 15),
+                            decoration: BoxDecoration(
+                                border:
+                                    Border.all(color: Colors.purple, width: 2)),
+                            padding: EdgeInsets.all(10),
+                            child: Text(
+                              '\$${tx.amount}',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                  color: Colors.purple),
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(tx.title, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                              Text(DateFormat('yyyy-MM-dd').format(tx.date), style: TextStyle(color: Colors.grey),)
+                            ],
+                          ),
+                        ],
+                      ),
+                    ))
+                .toList(),
           )
         ],
       ),
