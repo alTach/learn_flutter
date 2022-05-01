b36db4a3f85600d5b1117bfa47565f1affd0ca1f
turach <turach.aliev@flexistax.com>
2022-05-01 23:21:34 +0300

    add Scaffold: appBar and body

--- udemy/lib/main.dart
+++ udemy/lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Center(
-      child: Text(
-    'Hellow Flutter',
-    textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 30, color: Colors.red, fontWeight: FontWeight.bold),
-  )));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first app'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text(
+            'This is my home page',
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30),
+          ),
+        ),
+        backgroundColor: Colors.indigo,
+      ),
+    ),
+  );
 }
