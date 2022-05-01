123a3ca9f9a34521167cdc5b4c9c58883a85c496
turach <turach.aliev@flexistax.com>
2022-05-02 00:53:17 +0300

    acquainted with Statefull widget

--- udemy/lib/main.dart
+++ udemy/lib/main.dart
@@ -1,39 +1,82 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(MyFirstApp());
 }
 
-class MyFirstApp extends StatelessWidget {
+class MyFirstApp extends StatefulWidget {
+  @override
+  _MyFirstAppState createState() => _MyFirstAppState();
+}
+
+class _MyFirstAppState extends State<MyFirstApp> {
+  bool _loading = false;
+  double _progressValue = 0.0;
+
+  @override
+  void initState() {
+    _loading = false;
+    _progressValue = 0.4;
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.indigo,
         appBar: AppBar(
-          title: Text('asdasd'),
+          title: Text('My First App'),
           centerTitle: true,
           backgroundColor: Colors.red,
         ),
         body: Center(
           child: Container(
             padding: EdgeInsets.all(16),
-            child: Column(
+            child: _loading
+                ? Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                LinearProgressIndicator(value: 23, color: Colors.cyanAccent),
-                Text('23%', style: TextStyle(color: Colors.white)),
-                Text('Press button to download',
-                    style: TextStyle(color: Colors.white))
+              children: [
+                LinearProgressIndicator(
+                    value: _progressValue,
+                    color: Colors.cyanAccent),
+                Text('${(_progressValue * 100).round()}',
+                    style: TextStyle(color: Colors.white)),
               ],
+            )
+                : Text(
+              'Press button to download',
+              style: TextStyle(color: Colors.white),
             ),
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: null,
+          onPressed: () {
+            setState(() {
+              _loading = !_loading;
+              _updateProgress();
+            });
+          },
           child: Icon(Icons.download),
         ),
       ),
     );
+  }
+
+  void _updateProgress() {
+    const oneSec = const Duration(seconds: 1);
+    Timer.periodic(oneSec, (Timer timer) {
+      setState(() {
+        _progressValue += 0.2;
+        if (_progressValue.toStringAsFixed(1) == '1.0') {
+          _loading = false;
+          timer.cancel();
+          _progressValue = 0.0;
+          return;
+        }
+      });
+    });
   }
 }
