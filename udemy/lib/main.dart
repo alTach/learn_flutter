ba67b7443935671875e2977df801ead9af576f8e
turach <turach.aliev@flexistax.com>
2022-05-03 00:15:10 +0300

    acquainted with Stack widget

--- udemy/lib/main.dart
+++ udemy/lib/main.dart
@@ -16,11 +16,11 @@ class MyApp extends StatelessWidget {
           title: Text('Counter'),
         ),
         backgroundColor: Colors.indigo,
-        body: Row(children: [
-          Expanded(flex: 2,child: Image.network('https://angularscript.com/wp-content/uploads/2015/07/Angularjs-SVG-Icon-Directive.jpg')),
-          Expanded(flex: 3,child: Container(padding: EdgeInsets.all(30), color: Colors.yellow, child: Text('1'),)),
-          Expanded(flex: 2, child: Container(padding: EdgeInsets.all(30), color: Colors.green, child: Text('2'),)),
-          Expanded(child: Container(padding: EdgeInsets.all(30), color: Colors.red, child: Text('3'),)),
+        body: Stack(
+          alignment: Alignment.center,
+          children: [
+          Icon(Icons.tv, size: 500, color: Colors.red),
+          Positioned(child: Text('TV'), top: 50, left: 45,)
         ],),
         // body: Center(
         //   child: ,
