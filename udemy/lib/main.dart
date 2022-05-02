aec83c5e92642138a174c5c5d6fb94d28c0e5a67
turach <turach.aliev@flexistax.com>
2022-05-02 23:35:08 +0300

    create component counter

--- udemy/lib/main.dart
+++ udemy/lib/main.dart
@@ -3,98 +3,90 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 
 void main() {
-  return runApp(MyFirstApp());
+  return runApp(MyApp());
 }
 
-class MyFirstApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _MyFirstAppState createState() => _MyFirstAppState();
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Counter'),
+        ),
+        backgroundColor: Colors.indigo,
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('Tap + to increment'),
+              CounterWidget(),
+              Text('Tap - to decriment'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
-class _MyFirstAppState extends State<MyFirstApp> {
-  bool _loading = false;
-  double _progressValue = 0.0;
+class CounterWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _CounterWidgetState();
+  }
+}
+
+class _CounterWidgetState extends State<CounterWidget> {
+  int _state = 0;
 
   @override
   void initState() {
-    _loading = false;
-    _progressValue = 0.4;
-    super.initState();
+    _state = 0;
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(fontFamily: 'WaterBrush_Regular'),
-      home: Scaffold(
-        backgroundColor: Colors.indigo,
-        appBar: AppBar(
-          title: Text('My First App'),
-          centerTitle: true,
-          backgroundColor: Colors.red,
-        ),
-        body: Center(
-          child: Container(
-              padding: EdgeInsets.all(10),
-              child: _loading
-                  ? Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        LinearProgressIndicator(
-                            value: _progressValue, color: Colors.cyanAccent),
-                        Text('${(_progressValue * 100).round()}',
-                            style: TextStyle(color: Colors.white)),
-                      ],
-                    )
-                  : Center(
-                      child: Stack(
-                        fit: StackFit.expand,
-                        children: [
-                          Image(
-                            image: AssetImage('assets/images/bg.jpg'),
-                          ),
-                          Image.asset('assets/icons/icon.png'),
-                          Positioned(
-                            top: 16, left: 120,
-                            child: Text(
-                              'My custom fonts',
-                              style: TextStyle(
-                                  fontSize: 30,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                // fontFamily: 'WaterBrush_Regular'
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    )),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              _loading = !_loading;
-              _updateProgress();
-            });
-          },
-          child: Icon(Icons.download),
+    // TODO: implement build
+    return Container(
+      padding: EdgeInsets.all(5),
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(5), color: Colors.white),
+      child: Container(
+        color: Colors.white,
+        width: 100,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              icon: Icon(Icons.remove),
+              onPressed: () {
+                decrement();
+              },
+            ),
+            Text('${_state}'),
+            IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                increment();
+              },
+            ),
+          ],
         ),
       ),
     );
   }
 
-  void _updateProgress() {
-    const oneSec = const Duration(seconds: 1);
-    Timer.periodic(oneSec, (Timer timer) {
-      setState(() {
-        _progressValue += 0.2;
-        if (_progressValue.toStringAsFixed(1) == '1.0') {
-          _loading = false;
-          timer.cancel();
-          _progressValue = 0.0;
-          return;
-        }
-      });
+  increment() {
+    setState(() {
+      _state++;
+    });
+  }
+
+  decrement() {
+    setState(() {
+      _state--;
     });
   }
 }
