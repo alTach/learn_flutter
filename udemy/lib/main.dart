475b829ff3d1bb0183934e79ad1d4d4693f7b440
turach <turach.aliev@flexistax.com>
2022-05-04 23:30:38 +0300

    acquainted with simple navigation

--- udemy/lib/main.dart
+++ udemy/lib/main.dart
@@ -1,70 +1,55 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
-void main() {
-  return runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Counter'),
+          title: Text('Simple Routing'),
+          centerTitle: true,
         ),
-        // backgroundColor: Colors.indigo,
-        body: BodyListView(),
-        // body: Center(
-        //   child: ,
-        // ),
+        body: HomePage(),
       ),
     );
   }
 }
 
-class BodyListView extends StatelessWidget {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return _myListView();
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          Route route = MaterialPageRoute(builder: (context) => Page2());
+          Navigator.push(context, route);
+        },
+        child: Text('Move to Page 2'),
+      ),
+    );
   }
 }
 
-Widget _myListView() {
-  final List<ListItem> items = List.generate(10000, (index) =>
-  index % 6 == 0
-      ? HeadingItem('Heading ${index}')
-      : MessageItem('Sender ${index}', 'Message body $index'));
-  return ListView.builder(itemBuilder: (context, index) {
-      final item = items[index];
-      if (item is HeadingItem) {
-        return ListTile(title: Text(item.heading, style: Theme
-            .of(context)
-            .textTheme
-            .headline5,),);
-      } else if (item is MessageItem) {
-        return ListTile(title: Text(item.sender),
-          subtitle: Text(item.body),
-          trailing: Icon(Icons.arrow_right),
-          leading: Icon(Icons.insert_photo),);
-      }
-      return Text('sd');
-    }, itemCount: items.length,);
-}
-
-abstract class ListItem {}
-
-class HeadingItem implements ListItem {
-  final String heading;
-
-  HeadingItem(this.heading);
-}
-
-class MessageItem implements ListItem {
-  final String sender;
-  final String body;
-
-  MessageItem(this.sender, this.body);
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Simple Routing'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('Page 2'),
+          ),
+        ),
+      ),
+    );
+  }
 }
