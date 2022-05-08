d7599e0c8b20cc1fbc9d2c54aeef0791323f7380
turach <turach.aliev@flexistax.com>
2022-05-08 20:11:20 +0300

    acquainted with using dep

--- udemy/lib/main.dart
+++ udemy/lib/main.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
-import 'pages/register_form_pages.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var rowDate = DateTime(1485789600);
+    var dateFormat = DateFormat('EEEE');
+    print(dateFormat.format(rowDate));
     return MaterialApp(
         title: 'Flutter demo',
         theme: ThemeData(
             primarySwatch: Colors.blue,
             visualDensity: VisualDensity.adaptivePlatformDensity),
-        home: RegisterFormPages());
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(' Register Form'),
+          ),
+          body: Text("sdadas"),
+        ));
   }
 }
