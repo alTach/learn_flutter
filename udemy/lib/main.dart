b163418a3b64025102b00fae1dd23ce190a42b31
turach <turach.aliev@flexistax.com>
2022-05-09 22:33:01 +0300

    Vanilla (setState)

--- udemy/lib/main.dart
+++ udemy/lib/main.dart
@@ -1,56 +1,78 @@
 import 'package:flutter/material.dart';
-import 'package:udemy/offices.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Vanilla Demo', home: MyHomePage());
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  late Future<OfficesList> officesList;
+// class _MyAppState extends State<MyApp> {
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return
+// }
 
+class MyHomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State {
+  int _rating = 0;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter demo',
-        theme: ThemeData(
-            primarySwatch: Colors.blue,
-            visualDensity: VisualDensity.adaptivePlatformDensity),
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text(' Register Form'),
-          ),
-          body: Container(
-            child: FutureBuilder<OfficesList>(
-              future: officesList,
-              builder: (context, snapshort) {
-                if (snapshort.hasData) {
-                  return ListView.builder(
-                      itemCount: snapshort.data?.offices.length,
-                      itemBuilder: (context , index) {
-                        return Card(child: ListTile(
-                          title: Text(snapshort.data?.offices[index].name ?? ''),
-                          subtitle: Text(snapshort.data?.offices[index].address ?? ''),
-                          leading: Image.network(snapshort.data?.offices[index].image ?? ''),
-                          isThreeLine: true,
-                        ),);
-                      }
-                  );
-                } else  if(snapshort.hasError) {
-                  return Text('Error');
-                }
-                return Center(child: CircularProgressIndicator(),);
-              },
+    double _size = 50;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(' Vanilla Demo'),
+      ),
+      body: Center(
+        child: Row(
+          children: [
+            Container(
+              child: IconButton(
+                icon: _rating >= 1 ? Icon(Icons.stars) : Icon(Icons.star_border),
+                iconSize: _size,
+                color: Colors.indigo[500],
+                onPressed: () {
+                  setState(() {
+                    _rating = 1;
+                  });
+                },
+              ),
             ),
-          ),
-        ));
-  }
-
-  @override
-  void initState() {
-    officesList = getOfficeList();
+            Container(
+              child: IconButton(
+                icon: _rating >= 2 ? Icon(Icons.stars) : Icon(Icons.star_border),
+                iconSize: _size,
+                color: Colors.indigo[500],
+                onPressed: () {
+                  setState(() {
+                    _rating = 2;
+                  });
+                },
+              ),
+            ),
+            Container(
+              child: IconButton(
+                icon: _rating >= 3 ? Icon(Icons.stars) : Icon(Icons.star_border),
+                iconSize: _size,
+                color: Colors.indigo[500],
+                onPressed: () {
+                  setState(() {
+                    _rating = 3;
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
