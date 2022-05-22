c1d3912b1420d7336a70f083bdaaa356ddbb71d1
turach <turach.aliev@flexistax.com>
2022-05-22 13:41:27 +0300

    acquainted with bloc store version 2

--- udemy/lib/color_bloc.dart
+++ udemy/lib/color_bloc.dart
@@ -1,35 +1,43 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 enum ColorEvent { eventRed, eventGreen }
 
-class ColorBloc {
- Color _color = Colors.red;
- final _inputEventController = StreamController<ColorEvent>();
+class ColorBloc extends Cubit<Color> {
 
- StreamSink<ColorEvent> get inputEventSink => _inputEventController.sink;
+  ColorBloc(Color initialState) : super(initialState);
 
- final _outputStateController = StreamController<Color>();
- Stream<Color> get outputStateStream => _outputStateController.stream;
+  updateState(Color state) {
+    emit(state);
+  }
 
- void _mapEventToState(event) {
-   if (event == ColorEvent.eventRed) {
-     _color = Colors.red;
-   } else if (event == ColorEvent.eventGreen) {
-     _color = Colors.green;
-   } else {
-     throw Exception('Wrong event type');
-   }
-   _outputStateController.sink.add(_color);
- }
-
- ColorBloc() {
-   _inputEventController.stream.listen(_mapEventToState);
- }
-
- void dispose() {
-   _inputEventController.close();
-   _outputStateController.close();
- }
+ // Color _color = Colors.red;
+ // final _inputEventController = StreamController<ColorEvent>();
+ //
+ // StreamSink<ColorEvent> get inputEventSink => _inputEventController.sink;
+ //
+ // final _outputStateController = StreamController<Color>();
+ // Stream<Color> get outputStateStream => _outputStateController.stream;
+ //
+ // void _mapEventToState(event) {
+ //   if (event == ColorEvent.eventRed) {
+ //     _color = Colors.red;
+ //   } else if (event == ColorEvent.eventGreen) {
+ //     _color = Colors.green;
+ //   } else {
+ //     throw Exception('Wrong event type');
+ //   }
+ //   _outputStateController.sink.add(_color);
+ // }
+ //
+ // ColorBloc() {
+ //   _inputEventController.stream.listen(_mapEventToState);
+ // }
+ //
+ // void dispose() {
+ //   _inputEventController.close();
+ //   _outputStateController.close();
+ // }
 }
--- udemy/lib/main.dart
+++ udemy/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:udemy/color_bloc.dart';
 
 
@@ -10,60 +11,95 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter demo',
-      home: HomePage(),
+      home: BlocProvider(create: (_) => ColorBloc(Colors.red),
+      child: MyHomePage(),
+      ),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  @override
-  _StudentPageState createState() => _StudentPageState();
-}
-
-class _StudentPageState extends State<HomePage> {
-  ColorBloc _bloc = ColorBloc();
-
-
-  @override
-  void dispose() {
-    super.dispose();
-    _bloc.dispose();
-  }
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    ColorBloc _bloc = BlocProvider.of<ColorBloc>(context);
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Block with stream'),
-        centerTitle: true,
-        backgroundColor: Colors.black,
-      ),
+      appBar: AppBar(title: Text('Block with flutter block'),),
       body: Center(
-        child: StreamBuilder(
-          stream: _bloc.outputStateStream,
-          initialData: Colors.red,
-          builder: (context, AsyncSnapshot<Color> snapshot) {
-            return AnimatedContainer(
-              height: 100,
-              width: 100,
-              color: snapshot.data,
-              duration: const Duration(milliseconds: 500),
-            );
-          }
-        ),
+        child: BlocBuilder<ColorBloc, Color>(builder: (context, color) => AnimatedContainer(
+          height: 100,
+          width: 100,
+          color: color,
+          duration: const Duration(milliseconds: 500),
+        )),
       ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           FloatingActionButton(onPressed: (){
-            _bloc.inputEventSink.add(ColorEvent.eventRed);
+            _bloc.updateState(Colors.red);
           }, backgroundColor: Colors.red,),
           SizedBox(width: 50),
           FloatingActionButton(onPressed: (){
-            _bloc.inputEventSink.add(ColorEvent.eventGreen);
+            _bloc.updateState(Colors.green);
           }, backgroundColor: Colors.green,),
         ],
       ),
     );
   }
 }
+
+// class HomePage extends StatefulWidget {
+//   @override
+//   _StudentPageState createState() => _StudentPageState();
+// }
+//
+// class _StudentPageState extends State<HomePage> {
+//   ColorBloc _bloc = ColorBloc();
+//
+//
+//   @override
+//   void dispose() {
+//     super.dispose();
+//     _bloc.dispose();
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Block with stream'),
+//         centerTitle: true,
+//         backgroundColor: Colors.black,
+//       ),
+//       body: Center(
+//         child: StreamBuilder(
+//           stream: _bloc.outputStateStream,
+//           initialData: Colors.red,
+//           builder: (context, AsyncSnapshot<Color> snapshot) {
+//             return AnimatedContainer(
+//               height: 100,
+//               width: 100,
+//               color: snapshot.data,
+//               duration: const Duration(milliseconds: 500),
+//             );
+//           }
+//         ),
+//       ),
+//       floatingActionButton: Row(
+//         mainAxisAlignment: MainAxisAlignment.end,
+//         children: [
+//           FloatingActionButton(onPressed: (){
+//             _bloc.inputEventSink.add(ColorEvent.eventRed);
+//           }, backgroundColor: Colors.red,),
+//           SizedBox(width: 50),
+//           FloatingActionButton(onPressed: (){
+//             _bloc.inputEventSink.add(ColorEvent.eventGreen);
+//           }, backgroundColor: Colors.green,),
+//         ],
+//       ),
+//     );
+//   }
+// }
