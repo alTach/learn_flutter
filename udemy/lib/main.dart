41b72ea2ea349f1c412d7e9f546b2b1dbaee90e6
turach <turach.aliev@flexistax.com>
2022-06-13 16:17:22 +0300

    create new app

--- udemy/lib/main.dart
+++ udemy/lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
-import 'pages/home_page.dart';
+import 'pages/webview_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      home: WebViewPage(),
     );
   }
 }
---
+++ udemy/lib/pages/webview_page.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class WebViewPage extends StatefulWidget {
+  @override
+  WebViewExampleState createState() => WebViewExampleState();
+}
+
+class WebViewExampleState extends State<WebViewPage> {
+  @override
+  void initState() {
+    super.initState();
+    // Enable virtual display.
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl: 'https://flutter.dev',
+    );
+  }
+}
