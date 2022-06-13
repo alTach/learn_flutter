539b9979ec3caa9c8285f4dfb3a5b47269fec0ea
turach <turach.aliev@flexistax.com>
2022-06-13 19:13:14 +0300

    add navigation for webView

--- udemy/lib/pages/webview_page.dart
+++ udemy/lib/pages/webview_page.dart
@@ -1,37 +1,130 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class WebViewApp extends StatelessWidget {
+enum menuOptions {
+  clearCache,
+  clearCookie
+}
+
+class WebViewApp extends StatefulWidget {
   const WebViewApp({Key? key}) : super(key: key);
 
   @override
+  State<WebViewApp> createState() => _WebViewAppState();
+}
+
+class _WebViewAppState extends State<WebViewApp> {
+  late WebViewController _webController;
+  double loadingProgress = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('WebView'),
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.arrow_back_ios),
-            onPressed: () async {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.arrow_forward_ios),
-            onPressed: () async {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.replay),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: const WebView(
-          javascriptMode: JavascriptMode.unrestricted,
-          initialUrl: 'https://flutter.dev'
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.next_plan, size: 32),
-        onPressed: () async {},
-      ),
+    return WillPopScope(
+      onWillPop: () async {
+        // Stay in app
+        return false;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('WebView'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.arrow_back_ios),
+              onPressed: () async {
+                if (await _webController.canGoBack()) {
+                  _webController.goBack();
+                } else {
+                  log('Нет записи в истории');
+                }
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.arrow_forward_ios),
+              onPressed: () async {
+                if (await _webController.canGoForward()) {
+                  _webController.goForward();
+                } else {
+                  log('Нет записи в истории');
+                }
+              },
+            ),
+            IconButton(
+              icon: const Icon(Icons.replay),
+              onPressed: () => _webController.reload(),
+            ),
+            PopupMenuButton(
+                onSelected: (value) {
+                  switch(value) {
+                    case menuOptions.clearCache: {
+                      _onClearCache(_webController, context);
+                      break;
+                    }
+                    case menuOptions.clearCookie: {
+                      _onClearCookies(context);
+                      break;
+                    }
+                    default: {
+
+                    }
+                  }
+                },
+                itemBuilder: (context) => [
+              PopupMenuItem(
+                value: menuOptions.clearCache,
+                child: Text('Удалить кеш')
+              ),
+              PopupMenuItem(
+                  value: menuOptions.clearCookie,
+                  child: Text('Почистить куки')
+              ),
+            ])
+          ],
+        ),
+        body: Column(
+          children: [
+            LinearProgressIndicator(value: loadingProgress.toDouble(), color: Colors.red, backgroundColor: Colors.black,),
+            Expanded(
+              child: WebView(
+                onProgress: (int progress) {
+                  loadingProgress = progress / 100;
+                  setState(() {});
+                },
+                javascriptMode: JavascriptMode.unrestricted,
+                initialUrl: 'https://flutter.dev',
+                onWebViewCreated: (controller) {
+                  _webController = controller;
+                },
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.next_plan, size: 32),
+          onPressed: () async {
+            _webController.loadUrl('https://www.youtube.com/');
+          },
+        ),
+      )
     );
+  }
+
+  void _onClearCache(WebViewController webController, BuildContext context) async {
+    await webController.clearCache();;
+    String message = '';
+    message = 'Кеш почищен';
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
+  }
+
+  void _onClearCookies(BuildContext context) async {
+    final bool hasCookie = await CookieManager().clearCookies();
+    String message = '';
+    if (!hasCookie) {
+      message = 'Кук нет, нечего чистить';
+    } else {
+      message = 'Куки почищены';
+    }
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
   }
 }
