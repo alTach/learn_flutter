6852e4aa4c1cf1bb6c42d6cd00eeb237956b5540
turach <turach.aliev@flexistax.com>
2022-06-13 18:13:45 +0300

    add webview

--- udemy/lib/main.dart
+++ udemy/lib/main.dart
@@ -1,19 +1,10 @@
 import 'package:flutter/material.dart';
-import 'pages/webview_page.dart';
+import 'package:udemy/pages/webview_page.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData.dark(),
-      home: WebViewPage(),
-    );
-  }
+  runApp(
+    const MaterialApp(
+      home: WebViewApp(),
+    ),
+  );
 }
--- udemy/lib/pages/webview_page.dart
+++ udemy/lib/pages/webview_page.dart
@@ -1,25 +1,37 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class WebViewPage extends StatefulWidget {
-  @override
-  WebViewExampleState createState() => WebViewExampleState();
-}
-
-class WebViewExampleState extends State<WebViewPage> {
-  @override
-  void initState() {
-    super.initState();
-    // Enable virtual display.
-    if (Platform.isAndroid) WebView.platform = AndroidWebView();
-  }
+class WebViewApp extends StatelessWidget {
+  const WebViewApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return WebView(
-      initialUrl: 'https://flutter.dev',
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('WebView'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.arrow_back_ios),
+            onPressed: () async {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.arrow_forward_ios),
+            onPressed: () async {},
+          ),
+          IconButton(
+            icon: const Icon(Icons.replay),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: const WebView(
+          javascriptMode: JavascriptMode.unrestricted,
+          initialUrl: 'https://flutter.dev'
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.next_plan, size: 32),
+        onPressed: () async {},
+      ),
     );
   }
 }
